08a7039d89ef658a41f64611f22cd0a8b30a8f60
etsai <etsai-unknown@terracottatech.com>
2007-10-03 05:22:36 +0000

    exit reconnect loop if channel closed to avoid exception at shutdown
    svn path=/dso/branches/private/; revision=5778

--- active-active/code/base/dso-common/src/com/tc/net/protocol/transport/ClientConnectionEstablisher.java
+++ active-active/code/base/dso-common/src/com/tc/net/protocol/transport/ClientConnectionEstablisher.java
@@ -38,6 +38,7 @@ public class ClientConnectionEstablisher {
   private Thread                          connectionEstablisher;
 
   private NoExceptionLinkedQueue          reconnectRequest       = new NoExceptionLinkedQueue();  // <ConnectionRequest>
+  private final SynchronizedBoolean       quitReconnect          = new SynchronizedBoolean(false);
 
   public ClientConnectionEstablisher(TCConnectionManager connManager, ConnectionAddressProvider connAddressProvider,
                                      int maxReconnectTries, int timeout) {
@@ -128,6 +129,7 @@ public class ClientConnectionEstablisher {
       for (int i = 0; ((maxReconnectTries < 0) || (i < maxReconnectTries)) && !connected; i++) {
         ConnectionAddressIterator addresses = connAddressProvider.getIterator();
         while (addresses.hasNext() && !connected) {
+          if (quitReconnect.get()) return;
           TCConnection connection = null;
           final ConnectionInfo connInfo = addresses.next();
           try {
@@ -232,6 +234,7 @@ public class ClientConnectionEstablisher {
 
   public void quitReconnectAttempts() {
     putReconnectRequest(new ConnectionRequest(ConnectionRequest.QUIT, null));
+    quitReconnect.set(true);
   }
 
   static class AsyncReconnect implements Runnable {
