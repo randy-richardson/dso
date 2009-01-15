a973e6c38a4662fa5fca3f31804df4c677310be1
nelrahma <nelrahma-unknown@terracottatech.com>
2009-01-15 10:15:05 +0000

    serialize/deserialize tickertoken msg
    svn path=/dso/branches/private/; revision=11271

--- ap-dgc-multithreaded/code/base/dso-l2/src/com/tc/util/AbstractTickerToken.java
+++ ap-dgc-multithreaded/code/base/dso-l2/src/com/tc/util/AbstractTickerToken.java
@@ -4,22 +4,31 @@
  */
 package com.tc.util;
 
+import com.tc.io.TCByteBufferInput;
+import com.tc.io.TCByteBufferOutput;
+import com.tc.io.TCSerializable;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-public abstract class AbstractTickerToken implements TickerToken {
+public abstract class AbstractTickerToken implements TickerToken, TCSerializable {
 
-  protected final Map<Integer, Boolean> tokenStateMap;
-  protected final int                   primaryID;
-  protected final int                   primaryTickValue;
+  protected int                   primaryID;
+  protected int                   primaryTickValue;
+  protected Map<Integer, Boolean> tokenStateMap = new HashMap<Integer, Boolean>();
 
   public AbstractTickerToken(int primaryID, int primaryTickValue) {
-    this(primaryID, primaryTickValue, new HashMap<Integer, Boolean>());
+    this.primaryID = primaryID;
+    this.primaryTickValue = primaryTickValue;
   }
 
   public AbstractTickerToken(int primaryID, int primaryTickValue, Map<Integer, Boolean> tokenStateMap) {
-    this.primaryID = primaryID;
-    this.primaryTickValue = primaryTickValue;
+    this(primaryID, primaryTickValue);
     this.tokenStateMap = tokenStateMap;
   }
 
@@ -37,6 +46,54 @@ public abstract class AbstractTickerToken implements TickerToken {
 
   public Map<Integer, Boolean> getTokenStateMap() {
     return tokenStateMap;
+  }
+
+  public Object deserializeFrom(TCByteBufferInput serialInput) {
+    try {
+      primaryID = serialInput.readInt();
+      primaryTickValue = serialInput.readInt();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    tokenStateMap = (HashMap<Integer, Boolean>)deserializeObject(serialInput);
+    return this;
+  }
+
+  public void serializeTo(TCByteBufferOutput serialOutput) {
+    serialOutput.writeInt(primaryID);
+    serialOutput.writeInt(primaryTickValue);
+    serializeObject(serialOutput, tokenStateMap);
+  }
+  
+  protected void serializeObject(TCByteBufferOutput serialOutput, Object obj) {
+    ByteArrayOutputStream boas = new ByteArrayOutputStream();
+    try {
+      ObjectOutputStream oos = new ObjectOutputStream(boas);
+      oos.writeObject(obj);
+      oos.close();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    byte[] objectArray = boas.toByteArray();
+    serialOutput.writeInt(objectArray.length);
+    serialOutput.write(objectArray);
+  }
+
+  protected Object deserializeObject(TCByteBufferInput serialInput) {
+    Object obj = null;
+    try {
+      int objectArrayLength = serialInput.readInt();
+      byte[] objectArray = new byte[objectArrayLength];
+      serialInput.read(objectArray);
+      ByteArrayInputStream bais = new ByteArrayInputStream(objectArray);
+      ObjectInputStream ois = new ObjectInputStream(bais);
+      obj = ois.readObject();
+    } catch (ClassNotFoundException e) {
+      throw new AssertionError(e);
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    return obj;
   }
 
 }
