c1b8e08c77ef5af3b5686e59baf775ab24c290c6
rahul singh <rsingh-unknown@terracottatech.com>
2009-01-16 08:37:49 +0000

    removing debug message
    svn path=/dso/branches/private/; revision=11285

--- ap-dgc-multithreaded/code/base/dso-l2/src/com/tc/objectserver/persistence/sleepycat/ManagedObjectPersistorImpl.java
+++ ap-dgc-multithreaded/code/base/dso-l2/src/com/tc/objectserver/persistence/sleepycat/ManagedObjectPersistorImpl.java
@@ -552,9 +552,6 @@ public final class ManagedObjectPersistorImpl extends SleepycatPersistorBase imp
 
   private void validateID(ObjectID id) {
     Assert.assertNotNull(id);
-    if(ObjectID.NULL_ID.equals(id)){
-      System.out.println("looking for: " + id);
-    }
     Assert.eval(!ObjectID.NULL_ID.equals(id));
   }
 
