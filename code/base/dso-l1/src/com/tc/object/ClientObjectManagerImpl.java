e082782a34015e66bc3435b587575987af385ca4
Nihit Purwar <npurwar@terracottatech.com>
2010-10-22 07:35:38 +0000

    unsed import 3.3
    svn path=/dso/patches/patched_3.3.0/; revision=16526

--- code/base/dso-l1/src/com/tc/object/ClientObjectManagerImpl.java
+++ code/base/dso-l1/src/com/tc/object/ClientObjectManagerImpl.java
@@ -76,7 +76,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.Map.Entry;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Semaphore;
 
