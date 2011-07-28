9d93fa7dfd391611b9befd2407359bfd40a52a66
Abhishek Maheshwari <amaheshw@terracottatech.com>
2011-07-28 09:21:57 +0000

    pvt branch, LocalCache changes
    svn path=/dso/branches/private/; revision=18298

--- getAll/code/base/dso-l1-api/src/com/tc/object/TCObjectServerMap.java
+++ getAll/code/base/dso-l1-api/src/com/tc/object/TCObjectServerMap.java
@@ -115,7 +115,7 @@ public interface TCObjectServerMap<L> extends TCObject {
 
   public Map<Object, Object> getAllValuesUnlocked(final Map<ObjectID, Set<Object>> mapIdToKeysMap);
 
-  public void addInLocalCache(Object key, Object value);
+  public void updateLocalCache(final Object key, final Object value);
 
   /**
    * Returns a snapshot of keys for the giver ServerTCMap
