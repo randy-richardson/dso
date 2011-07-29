bd4ef062463b7e84d0fcafdac42ef06e1c7d299b
tim wu <twu@terracottatech.com>
2011-07-29 19:10:06 +0000

    Priv-inline-dgc: Merged changes from trunk
    svn path=/dso/branches/private/; revision=18330

--- inline-dgc/code/base/dso-l2/src/com/tc/objectserver/impl/DistributedObjectServer.java
+++ inline-dgc/code/base/dso-l2/src/com/tc/objectserver/impl/DistributedObjectServer.java
@@ -256,7 +256,9 @@ import com.tc.objectserver.tx.TransactionalObjectManagerImpl;
 import com.tc.objectserver.tx.TransactionalStagesCoordinatorImpl;
 import com.tc.objectserver.tx.TxnsInSystemCompletionListener;
 import com.tc.operatorevent.DsoOperatorEventHistoryProvider;
+import com.tc.operatorevent.TerracottaOperatorEventFactory;
 import com.tc.operatorevent.TerracottaOperatorEventHistoryProvider;
+import com.tc.operatorevent.TerracottaOperatorEventLogger;
 import com.tc.operatorevent.TerracottaOperatorEventLogging;
 import com.tc.properties.L1ReconnectConfigImpl;
 import com.tc.properties.ReconnectConfig;
@@ -1191,7 +1193,7 @@ public class DistributedObjectServer implements TCDumper, LockInfoDumpHandler, S
       dgcSequenceProvider.registerSequecePublisher(this.l2Coordinator.getReplicatedClusterStateManager());
     } else {
       this.l2State.setState(StateManager.ACTIVE_COORDINATOR);
-      this.l2Coordinator = new L2HADisabledCooridinator(this.groupCommManager);
+      this.l2Coordinator = new L2HADisabledCooridinator(this.groupCommManager, this.persistor.getPersistentStateStore());
     }
 
     this.dumpHandler.registerForDump(new CallbackDumpAdapter(this.l2Coordinator));
@@ -1242,6 +1244,11 @@ public class DistributedObjectServer implements TCDumper, LockInfoDumpHandler, S
 
     startGroupManagers();
     this.l2Coordinator.start();
+    if (!this.l2Coordinator.isStartedWithCleanDB() && this.l2Coordinator.getStateManager().isActiveCoordinator()) {
+      TerracottaOperatorEventLogger operatorEventLogger = TerracottaOperatorEventLogging.getEventLogger();
+      operatorEventLogger.fireOperatorEvent(TerracottaOperatorEventFactory
+          .createActiveServerWithOldDataBaseEvent(l2DSOConfig.serverName()));
+    }
     if (!networkedHA) {
       // In non-network enabled HA, Only active server reached here.
       startActiveMode();
