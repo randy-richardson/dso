9e5a9ca3df42d53e3e87def19958f5f43fa38e8c
vitali funshste <vfunshte-unknown@terracottatech.com>
2012-02-29 22:23:07 +0000

    Modified hashing to stage queues to take into account relative frequency, thereby distributing indexes evenly among worker threads.
    svn path=/dso/branches/private/; revision=19904

--- search/dso-l2/src/main/java/com/tc/objectserver/search/BaseSearchEventContext.java
+++ search/dso-l2/src/main/java/com/tc/objectserver/search/BaseSearchEventContext.java
@@ -9,6 +9,11 @@ import com.tc.objectserver.metadata.MetaDataProcessingContext;
 import com.tc.properties.TCPropertiesConsts;
 import com.tc.properties.TCPropertiesImpl;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
 class BaseSearchEventContext implements SearchEventContext, MultiThreadedEventContext {
 
   private static final int                INDEX_PER_CACHE     = TCPropertiesImpl
@@ -21,18 +26,74 @@ class BaseSearchEventContext implements SearchEventContext, MultiThreadedEventCo
   private final String                    cacheName;
   private final ObjectID                  segmentOid;
 
+  private int                             hashCode;
+
+  private final static class StageQFreq {
+    private final int qId;
+    private int       occurCt = 0;
+
+    private StageQFreq(int bucket) {
+      qId = bucket;
+    }
+  }
+
+  private static final ConcurrentMap<BaseSearchEventContext, Integer> buckets = new ConcurrentHashMap<BaseSearchEventContext, Integer>(
+                                                                                                                                       SEDA_SEARCH_THREADS);
+  private static final PriorityQueue<StageQFreq>                      tPool   = new PriorityQueue<StageQFreq>(
+                                                                                                              SEDA_SEARCH_THREADS,
+                                                                                                              new Comparator<StageQFreq>() {
+                                                                                                                public int compare(StageQFreq one,
+                                                                                                                                   StageQFreq other) {
+                                                                                                                  return one.occurCt < other.occurCt ? -1
+                                                                                                                      : (one.occurCt == other.occurCt ? 0
+                                                                                                                          : 1);
+                                                                                                                }
+                                                                                                              });
+  static {
+    for (int i = 0; i < SEDA_SEARCH_THREADS; i++) {
+      tPool.add(new StageQFreq(i));
+    }
+  }
+
   public BaseSearchEventContext(ObjectID segmentOid, String cacheName, MetaDataProcessingContext metaDataContext) {
     this.segmentOid = segmentOid;
     this.cacheName = cacheName;
     this.metaDataContext = metaDataContext;
+
+    computeHashCode();
   }
 
   public final Object getKey() {
-    // Pick the start thread index using cache name
-    int threadStart = Math.abs(cacheName.hashCode()) % SEDA_SEARCH_THREADS;
-    // Pick the next n threads for n indexes using segment id
-    int indexSegment = (int) (Math.abs(segmentOid.toLong()) % INDEX_PER_CACHE);
-    return (threadStart + indexSegment) % SEDA_SEARCH_THREADS;
+
+    Integer dest = buckets.get(this); // careful with autoboxing!
+    if (dest == null) {
+
+      StageQFreq lowest = tPool.peek();
+      dest = lowest.qId;
+      Integer tmp = buckets.putIfAbsent(this, dest);
+      if (tmp == null) {
+        tPool.poll();
+        lowest.occurCt++;
+        assert tPool.size() < SEDA_SEARCH_THREADS;
+        tPool.add(lowest);
+      } else {
+        dest = tmp;
+      }
+    }
+
+    return dest;
+  }
+
+  @Override
+  public int hashCode() {
+    return hashCode;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof BaseSearchEventContext)) return false;
+    BaseSearchEventContext other = (BaseSearchEventContext) obj;
+    return hashCode == other.hashCode;
   }
 
   public MetaDataProcessingContext getMetaDataProcessingContext() {
@@ -45,5 +106,14 @@ class BaseSearchEventContext implements SearchEventContext, MultiThreadedEventCo
 
   public ObjectID getSegmentOid() {
     return segmentOid;
+  }
+
+  private void computeHashCode() {
+    // Pick the start thread index using cache name
+    int threadStart = Math.abs(cacheName.hashCode()) % SEDA_SEARCH_THREADS;
+    // Pick the next n threads for n indexes using segment id
+    int indexSegment = (int) (Math.abs(segmentOid.toLong()) % INDEX_PER_CACHE);
+    hashCode = (threadStart + indexSegment) % SEDA_SEARCH_THREADS;
+
   }
 }
