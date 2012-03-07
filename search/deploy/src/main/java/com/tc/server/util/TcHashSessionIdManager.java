c4d740ff98c0affb25a9d33a0841bd59875d265d
rahul singh <rsingh-unknown@terracottatech.com>
2012-03-07 02:06:07 +0000

    pulling in changes from trunk
    svn path=/dso/branches/private/; revision=19926

--- search/deploy/src/main/java/com/tc/server/util/TcHashSessionIdManager.java
+++ search/deploy/src/main/java/com/tc/server/util/TcHashSessionIdManager.java
@@ -20,17 +20,18 @@ import javax.servlet.http.HttpSession;
  * A copy of Jetty's HashSessionIdManager which does not use the SecureRandom instance during initialization
  */
 public class TcHashSessionIdManager extends AbstractLifeCycle implements SessionIdManager {
-  private final static String __NEW_SESSION_ID = "org.mortbay.jetty.newSessionId";
-  protected final static String SESSION_ID_RANDOM_ALGORITHM = "SHA1PRNG";
+  private final static String   __NEW_SESSION_ID                = "org.mortbay.jetty.newSessionId";
+  protected final static String SESSION_ID_RANDOM_ALGORITHM     = "SHA1PRNG";
   protected final static String SESSION_ID_RANDOM_ALGORITHM_ALT = "IBMSecureRandom";
 
-  MultiMap _sessions;
-  protected Random _random;
-  private boolean _weakRandom;
-  private String _workerName;
+  MultiMap                      _sessions;
+  protected Random              _random;
+  private boolean               _weakRandom;
+  private String                _workerName;
 
   /* ------------------------------------------------------------ */
   public TcHashSessionIdManager() {
+    //
   }
 
   /* ------------------------------------------------------------ */
@@ -42,9 +43,9 @@ public class TcHashSessionIdManager extends AbstractLifeCycle implements Session
   /* ------------------------------------------------------------ */
 
   /**
-   * Get the workname. If set, the workername is dot appended to the session
-   * ID and can be used to assist session affinity in a load balancer.
-   *
+   * Get the workname. If set, the workername is dot appended to the session ID and can be used to assist session
+   * affinity in a load balancer.
+   * 
    * @return String or null
    */
   public String getWorkerName() {
@@ -54,9 +55,9 @@ public class TcHashSessionIdManager extends AbstractLifeCycle implements Session
   /* ------------------------------------------------------------ */
 
   /**
-   * Set the workname. If set, the workername is dot appended to the session
-   * ID and can be used to assist session affinity in a load balancer.
-   *
+   * Set the workname. If set, the workername is dot appended to the session ID and can be used to assist session
+   * affinity in a load balancer.
+   * 
    * @param workerName
    */
   public void setWorkerName(String workerName) {
@@ -67,17 +68,15 @@ public class TcHashSessionIdManager extends AbstractLifeCycle implements Session
 
   /**
    * Get the session ID with any worker ID.
-   *
+   * 
    * @param request
    * @return sessionId plus any worker ID.
    */
   public String getNodeId(String clusterId, HttpServletRequest request) {
-    String worker = request == null ? null : (String)request.getAttribute("org.mortbay.http.ajp.JVMRoute");
-    if (worker != null)
-      return clusterId + '.' + worker;
+    String worker = request == null ? null : (String) request.getAttribute("org.mortbay.http.ajp.JVMRoute");
+    if (worker != null) return clusterId + '.' + worker;
 
-    if (_workerName != null)
-      return clusterId + '.' + _workerName;
+    if (_workerName != null) return clusterId + '.' + _workerName;
 
     return clusterId;
   }
@@ -86,7 +85,7 @@ public class TcHashSessionIdManager extends AbstractLifeCycle implements Session
 
   /**
    * Get the session ID with any worker ID.
-   *
+   * 
    * @param nodeId
    * @return sessionId plus any worker ID.
    */
@@ -96,6 +95,7 @@ public class TcHashSessionIdManager extends AbstractLifeCycle implements Session
   }
 
   /* ------------------------------------------------------------ */
+  @Override
   protected void doStart() {
     if (_random == null) {
       try {
@@ -115,24 +115,24 @@ public class TcHashSessionIdManager extends AbstractLifeCycle implements Session
   }
 
   /* ------------------------------------------------------------ */
+  @Override
   protected void doStop() {
-    if (_sessions != null)
-      _sessions.clear(); // Maybe invalidate?
+    if (_sessions != null) _sessions.clear(); // Maybe invalidate?
     _sessions = null;
   }
 
   /* ------------------------------------------------------------ */
   /*
-  * @see org.mortbay.jetty.SessionManager.MetaManager#idInUse(java.lang.String)
-  */
+   * @see org.mortbay.jetty.SessionManager.MetaManager#idInUse(java.lang.String)
+   */
   public boolean idInUse(String id) {
     return _sessions.containsKey(id);
   }
 
   /* ------------------------------------------------------------ */
   /*
-  * @see org.mortbay.jetty.SessionManager.MetaManager#addSession(javax.servlet.http.HttpSession)
-  */
+   * @see org.mortbay.jetty.SessionManager.MetaManager#addSession(javax.servlet.http.HttpSession)
+   */
   public void addSession(HttpSession session) {
     synchronized (this) {
       _sessions.add(getClusterId(session.getId()), session);
@@ -141,8 +141,8 @@ public class TcHashSessionIdManager extends AbstractLifeCycle implements Session
 
   /* ------------------------------------------------------------ */
   /*
-  * @see org.mortbay.jetty.SessionManager.MetaManager#addSession(javax.servlet.http.HttpSession)
-  */
+   * @see org.mortbay.jetty.SessionManager.MetaManager#addSession(javax.servlet.http.HttpSession)
+   */
   public void removeSession(HttpSession session) {
     synchronized (this) {
       _sessions.removeValue(getClusterId(session.getId()), session);
@@ -151,14 +151,14 @@ public class TcHashSessionIdManager extends AbstractLifeCycle implements Session
 
   /* ------------------------------------------------------------ */
   /*
-  * @see org.mortbay.jetty.SessionManager.MetaManager#invalidateAll(java.lang.String)
-  */
+   * @see org.mortbay.jetty.SessionManager.MetaManager#invalidateAll(java.lang.String)
+   */
   public void invalidateAll(String id) {
     synchronized (this) {
       // Do not use interators as this method tends to be called recursively
       // by the invalidate calls.
       while (_sessions.containsKey(id)) {
-        AbstractSessionManager.Session session = (AbstractSessionManager.Session)_sessions.getValue(id, 0);
+        AbstractSessionManager.Session session = (AbstractSessionManager.Session) _sessions.getValue(id, 0);
 
         boolean isValid = true;
         try {
@@ -167,23 +167,20 @@ public class TcHashSessionIdManager extends AbstractLifeCycle implements Session
           isValid = false;
         }
 
-        if (isValid)
-          session.invalidate();
-        else
-          _sessions.removeValue(id, session);
+        if (isValid) session.invalidate();
+        else _sessions.removeValue(id, session);
       }
     }
   }
 
   /* ------------------------------------------------------------ */
   /*
-  * new Session ID. If the request has a requestedSessionID which is unique,
-  * that is used. The session ID is created as a unique random long XORed with
-  * connection specific information, base 36.
-  * @param request
-  * @param created
-  * @return Session ID.
-  */
+   * new Session ID. If the request has a requestedSessionID which is unique, that is used. The session ID is created as
+   * a unique random long XORed with connection specific information, base 36.
+   * @param request
+   * @param created
+   * @return Session ID.
+   */
   public String newSessionId(HttpServletRequest request, long created) {
     synchronized (this) {
       // A requested session ID can only be used if it is in use already.
@@ -191,26 +188,21 @@ public class TcHashSessionIdManager extends AbstractLifeCycle implements Session
 
       if (requested_id != null) {
         String cluster_id = getClusterId(requested_id);
-        if (idInUse(cluster_id))
-          return cluster_id;
+        if (idInUse(cluster_id)) return cluster_id;
       }
 
       // Else reuse any new session ID already defined for this request.
-      String new_id = (String)request.getAttribute(__NEW_SESSION_ID);
-      if (new_id != null && idInUse(new_id))
-        return new_id;
+      String new_id = (String) request.getAttribute(__NEW_SESSION_ID);
+      if (new_id != null && idInUse(new_id)) return new_id;
 
       // pick a new unique ID!
       String id = null;
       while (id == null || id.length() == 0 || idInUse(id)) {
-        long r = _weakRandom
-            ? (hashCode() ^ Runtime.getRuntime().freeMemory() ^ _random.nextInt() ^ (((long)request.hashCode()) << 32))
-            : _random.nextLong();
+        long r = _weakRandom ? (hashCode() ^ Runtime.getRuntime().freeMemory() ^ _random.nextInt() ^ (((long) request
+            .hashCode()) << 32)) : _random.nextLong();
         r ^= created;
-        if (request != null && request.getRemoteAddr() != null)
-          r ^= request.getRemoteAddr().hashCode();
-        if (r < 0)
-          r = -r;
+        if (request.getRemoteAddr() != null) r ^= request.getRemoteAddr().hashCode();
+        if (r < 0) r = -r;
         id = Long.toString(r, 36);
       }
 
