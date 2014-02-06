5467a3ec3fab4f9b2dee43415c08f4738279ff78
Gary Keim <gary.keim@softwareag.com>
2014-02-06 01:50:26 +0000

    updated for patched_4.0.04
    svn path=/dso/patches/patched_4.0.4/; revision=24476

--- devwork/setup.sh
+++ devwork/setup.sh
@@ -1,6 +1,6 @@
 # 4.0: https://svn.terracotta.org/repo/internal/enterprise/branches/4.0
-svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/branches/tc-4.0 management-apps
-svn co https://svn.terracotta.org/repo/ehcache/branches/ehcache-2.7.x ehcache
-svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/branches/ehcache-core-ee-2.7.x ehcache-ee
+svn co https://svn.terracotta.org/repo/forge/enterprise/management-apps/branches/patched_4.0.4 management-apps
+svn co https://svn.terracotta.org/repo/ehcache/patches/patched_2.7.4 ehcache
+svn co https://svn.terracotta.org/repo/internal/ehcache-enterprise/ehcache-core-ee/patches/patched_ehcache-core-ee-2.7.4 ehcache-ee
 # svn co https://svn.terracotta.org/repo/quartz/trunk quartz
 # svn co https://svn.terracotta.org/repo/forge/enterprise/quartz-ee/trunk quartz-ee
