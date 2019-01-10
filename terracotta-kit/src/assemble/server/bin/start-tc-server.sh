6badfea0d2d07b9a7b104aaa974d89b835a9d6b3
Madduri Venkata Sairam <VenkataSairam.Madduri@softwareag.com>
2019-01-10 06:44:39 +0000

    Add safe-mode option to start-tc-server script's help
    svn path=/dso/trunk/; revision=27641

--- terracotta-kit/src/assemble/server/bin/start-tc-server.sh
+++ terracotta-kit/src/assemble/server/bin/start-tc-server.sh
@@ -21,9 +21,11 @@ case "$1" in
   "--help"|"-h"|"-?")
     echo "Syntax: $0 [-f /path/to/tc-config.xml] [-n server_name] [--safe-mode]"
     echo
-    echo "-f : start the server with your own Terracotta configuration instead of the default one"
-    echo "-n : specify which server you want to start when you have more than one servers configured"
+    echo "         -f : start the server with your own Terracotta configuration instead of the default one"
+    echo "         -n : specify which server you want to start when you have more than one servers configured"
     echo "--safe-mode : start the server in safe mode"
+    echo
+    echo "NOTE: An external trigger will be required to resume operations if server is started in safe mode"
     exit
     ;;
 esac
