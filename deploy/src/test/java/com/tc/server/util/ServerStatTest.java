f3e9d5b2efc6fee53c7aaf85794f4737bb96565d
Gary Keim <gkeim@softwareag.com>
2023-01-24 20:45:17 -0500

    Web tools should retry on exception. Shortened some timeouts to avoid config-fetching ping-pong in crash tests.

--- deploy/src/test/java/com/tc/server/util/ServerStatTest.java
+++ deploy/src/test/java/com/tc/server/util/ServerStatTest.java
@@ -18,9 +18,9 @@ package com.tc.server.util;
 
 import org.junit.Test;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -64,30 +64,45 @@ public class ServerStatTest {
   @Test
   public void testUnknownError() throws Exception {
     String errorMessage = "this is an error message 29138491283749skjafhdkasj";
+    String stacktrace = "this is the stacktrace";
+    Map<String, String> response = new HashMap<>();
+    response.put("error", errorMessage);
+    response.put("stackTrace", stacktrace);
     WebTarget target = mockWebTarget("host", 4321);
     when(target.request(MediaType.APPLICATION_JSON_TYPE).get().getStatus()).thenReturn(403);
     when(target.request(MediaType.APPLICATION_JSON_TYPE)
         .get()
-        .readEntity(any(Class.class))).thenReturn(Collections.singletonMap("error", errorMessage));
-    ServerStat stats = ServerStat.getStats(target);
-    assertErrorStats(stats);
-    assertThat(stats.toString(), containsString(errorMessage));
+        .readEntity(any(Class.class))).thenReturn(response);
+    try {
+      ServerStat.getStats(target);
+      throw new AssertionError("Expected IOException");
+    } catch (IOException ioe) {
+      assertThat(ioe.getMessage(), containsString(errorMessage));
+    }
   }
 
   @Test
   public void testFourOhFour() throws Exception {
     WebTarget target = mockWebTarget("host", 4321);
     when(target.request(MediaType.APPLICATION_JSON_TYPE).get().getStatus()).thenReturn(404);
-    ServerStat stats = ServerStat.getStats(target);
-    assertErrorStats(stats);
+    try {
+      ServerStat stats = ServerStat.getStats(target);
+      throw new AssertionError("Expected IOException");
+    } catch (IOException ioe) {
+      assertThat(ioe.getMessage(), containsString("Unable to get status"));
+    }
   }
 
   @Test
   public void testAuthenticationFailure() throws Exception {
     WebTarget target = mockWebTarget("localhost", 1234);
     when(target.request(MediaType.APPLICATION_JSON_TYPE).get().getStatus()).thenReturn(401);
-    ServerStat stats = ServerStat.getStats(target);
-    assertErrorStats(stats);
+    try {
+      ServerStat.getStats(target);
+      throw new AssertionError("Expected IOException");
+    } catch (IOException ioe) {
+      assertThat(ioe.getMessage(), containsString("Authentication error"));
+    }
   }
 
   private void assertErrorStats(ServerStat stats) {
